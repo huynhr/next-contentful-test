04f5ae87581c0e22fa49c5a1b9911db59046ebaf
Ray Huynh <huynh.r.k@gmail.com>
2020-04-30 10:27:39 -0600

    Update with error page

--- pages/[page].js
+++ pages/[page].js
@@ -5,16 +5,21 @@ import { renderComponent } from '../lib/renderComponent'
 const TestPage = (props) => {
 
   console.log('props', props)
+  const content = Object.keys(props.data).length ? (
+    <React.Fragment>
+      <h1>{props.data.title}</h1>
+        {
+          props.data.blocks.map((item, i) => {
+            return renderComponent(item.fields.slug, i)
+          })
+        }
+        <Link href='/'><a>Go Home</a></Link>
+    </React.Fragment>
+  ) : (<h1>404 Error</h1>)
 
   return (
     <React.Fragment>
-      <h1>{props.data.title}</h1>
-      {
-        props.data.blocks.map((item, i) => {
-          return renderComponent(item.fields.slug, i)
-        })
-      }
-      <Link href='/'><a>Go Home</a></Link>
+      {content}
     </React.Fragment>
   )
 }
@@ -22,7 +27,7 @@ const TestPage = (props) => {
 export async function getServerSideProps(context) {
   const client = new ContentfulHandler()
   const response = await client.getEntries('page', { name: 'slug', value: context.params.page })
-  const item = response.items.length ? response.items[0].fields : []
+  const item = response.items.length ? response.items[0].fields : {}
   return {
     props: {
       data: item,
